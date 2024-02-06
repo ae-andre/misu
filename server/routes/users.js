7c6a6d8160f9aa22f3e9e5d332c2df83f306e275
ae-andre <aendique@gmail.com>
2024-02-06 09:24:20 -0700

    created contexts

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const User = require('../models/user');
 const verifyToken = require('../middleware/verifyToken');
 
-// Example: Get current user's profile
+// Get current user's profile
 router.get('/profile', verifyToken, async (req, res) => {
     try {
         const user = await User.findById(req.user.id).select('-password');
