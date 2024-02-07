59affb0690edc276bd13ddec42977f74c120d3c0
ae-andre <aendique@gmail.com>
2024-02-06 20:24:28 -0700

    fixed server start, login/signup working

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: false },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   friends: [{ type: Schema.Types.ObjectId, ref: 'User', max: 5 }] // Reference to other User documents
